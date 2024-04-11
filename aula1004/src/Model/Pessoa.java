837bc69d078eef59165c91ceeef4a7cf335a3533
Davi Campos <126365210+DaviOCampos@users.noreply.github.com>
2024-04-10 21:17:19 -0300

    Add files via upload

---
+++ aula1004/src/Model/Pessoa.java
@@ -0,0 +1,24 @@
+package Model;
+
+import java.util.Date;
+
+public class Pessoa {
+    String nome;
+    Date dataNascimento;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+
+
+    
+}
